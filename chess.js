a917d79723ffec8a013259919d8e4e71c5d21a01
ori shohat <orishohat@h-MBP-sl-ori.lan>
2022-04-17 17:17:19 +0300

    chess7

--- chess.js
+++ chess.js
@@ -89,3 +89,52 @@ for(i = 1; i < 9; i++){
     }
     chessBord.appendChild(box8)
 }
+
+function addimage(cell, type, name){
+    const image = document.createElement('img');
+    image.src = 'images/' + type + '/' + name + '.png';
+    cell.appendChild(image);
+  }
+
+
+
+function addImageByIndex(cell, type, index) {
+    if (index === 0 || index === 7) {
+      addImage(cell, type, 'rook');
+    } else if (index === 1 || index === 6) {
+      addImage(cell, type, 'knight');
+    } else if (index === 2 || index === 5) {
+      addImage(cell, type, 'bishop');
+    } else if (index === 3) {
+      addImage(cell, type, 'king');
+    } else if (index === 4) {
+      addImage(cell, type, 'queen');
+    }
+  }
+  
+  function chessBoard() {
+    const table1 = document.createElement('table');
+    document.body.appendChild(table1);
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      const row = table1.insertRow();
+      for (let j = 0; j < BOARD_SIZE; j++) {
+        const cell = row.insertCell();
+        cell.id = "cell-" + i.toString() + "_" + j.toString();
+        if ((i + j) % 2 === 0) {
+          cell.className = 'white';
+        } else {
+          cell.className = 'black';
+        }
+  
+        if (i === 0) {
+          addImageByIndex(cell, WHITE_TYPE, j);
+        } else if (i === 1) {
+          addImage(cell, WHITE_TYPE, 'pawn');
+        } else if (i === 6) {
+          addImage(cell, DARK_TYPE, 'pawn');
+        } else if (i === 7) {
+          addImageByIndex(cell, DARK_TYPE, j);
+        }
+      }
+    }
+  }
