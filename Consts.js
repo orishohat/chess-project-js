59fab2cdd77dfb15d9e8e25d1a692295b459ae20
ori shohat <orishohat@h-MBP-sl-ori.lan>
2022-04-28 11:16:49 +0300

    chess31

---
+++ Consts.js
@@ -0,0 +1,14 @@
+const BOARD_SIZE = 8;
+const WHITE_PLAYER = 'white';
+const DARK_PLAYER = 'dark';
+
+const PAWN = 'pawn';
+const ROOK = 'rook';
+const KNIGHT = 'knight';
+const BISHOP = 'bishop';
+const KING = 'king';
+const QUEEN = 'queen';
+
+const PIECES = [ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK];
+
+const CHESS_BOARD_ID = 'chess-board';
